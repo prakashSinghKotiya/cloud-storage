7f95a170fc5ab352a9a0e667768f2e2677587a76
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-07-05 20:31:24 +0530

    redis url update

--- server/database/redis.js
+++ server/database/redis.js
@@ -3,8 +3,8 @@ import {createClient} from "redis";
 
 const redisDb = createClient({
     url: process.env.REDIS_URL,
-    username: "default",
-    password: process.env.REDIS_PASS
+    // username: "default",
+    // password: process.env.REDIS_PASS
 })
 
 redisDb.on("error", (err) =>{ console.log("Redis Client Error", err);  process.exit(1)})
