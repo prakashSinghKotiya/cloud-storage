c106b88b3643199c4463831a0a335160125fb742
Prakash Kotiya <prakashkotiya100@gmail.com>
2026-07-07 23:17:03 +0530

    name cahnged

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppProviders from "./app/providers";
+import AppProviders from "./app/Providers";
 import "./styles/globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
