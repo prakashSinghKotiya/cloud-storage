78c9f9b35f46a1863a577dfcf5f573806cb9a044
Prakash Kotiya <prakashkotiya100@gmail.com>
2026-07-10 16:26:02 +0530

    fixed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router()
 
 router.post("/register", registerUser)
 
-router.post("/login",checkAuth,loginUser )
+router.post("/login",loginUser )
 
 router.get("/",checkAuth, userDetails)
 
